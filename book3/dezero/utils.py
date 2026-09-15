4d34fc83a6268ee7cf8c4143bb93d9258d92fa27
644731871zzz <644731871zzz@gmail.com>
2026-09-15 21:09:04 +0800

    20260915

--- book3/dezero/utils.py
+++ book3/dezero/utils.py
@@ -75,4 +75,38 @@ def get_dot_graph(output,verbose = True):
 def plot_dot_graph(output,verbose = True,to_file = 'graph.png'):
     dot_graph = get_dot_graph(output,verbose)
 
-    tmp_dir = 
+    #expanduser是展开用户的意思
+    #expanduser和~搭配,返回用户目录的字符串 User\Arch. (主目录)
+    #用join拼接起来,这是一个跨平台工具,可以识别添加不同系统的文件夹分隔符
+        #创建了.dezero的文件夹,存放缓存
+    #join只负责拼接,所以可能没有文件夹
+    tmp_dir = os.path.join(os.path.expanduser('~'),'.dezero')
+    #exist判断是否存在
+    if not os.path.exists(tmp_dir):
+        #mkdir 是 make directory 创建文件夹的意思
+        #识别的路径名,可以创建文件夹,文件夹的名字可以带.如aaa.aaa,识别的还是文件夹
+        #.aaa是特殊的隐藏文件夹
+        os.mkdir(tmp_dir)
+    #一样是拼接,只是得到的字符串,没有创建文件
+    graph_path = os.path.join(tmp_dir,'tmp_graph.dot')
+
+    #open这读取了字符串已经在对应位置创建了文件了
+    #open和mkdir都只创建目录最后的文件和文件夹,如果前面的路径没有会报错
+    with open(graph_path,'w') as f:
+        f.write(dot_graph)
+
+
+    #splitext将字符串拆分成一个名字和字符串后缀 返回一个元组
+        #[1]取出来的是第二项'.png' [1:]继续取出'png'
+    extension = os.path.splitext(to_file)[1][1:]
+    #graph_path是读取的dot文件 extension是写入的二进制的格式 to_file是输出文件名
+    cmd = 'dot {} -T {} -o {}'.format(graph_path,extension,to_file)
+    subprocess.run(cmd,shell = True)
+
+    try:
+        from IPython import display
+        #在jupyter命令行显示图像
+        return display.Image(filename = to_file)
+
+    except:
+        pass
